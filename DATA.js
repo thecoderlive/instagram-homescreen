4ee5dca24764b64128535ad65103c213bfe6b541
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-29 17:42:59 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,62 @@
+export const item = {
+    "id": "4704ac94-b55d-4bb2-8e8c-03a18e996ffe",
+    "stories": [
+        {
+            "id": "33da32de-a8aa-4787-97f1-98f69bef23ce",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/221.png",
+            "story_owner": "land"
+        },
+        {
+            "id": "6114e698-6304-4be8-92f1-daa0b0c437bb",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png",
+            "story_owner": "different"
+        },
+        {
+            "id": "d16f45ca-71c9-4437-893b-f1df554c32d6",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/252.png",
+            "story_owner": "course"
+        },
+        {
+            "id": "2d441ebf-4708-4434-b685-3ea16b0b688f",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/288.png",
+            "story_owner": "four"
+        },
+        {
+            "id": "6ba1081e-8c33-4c46-bdf6-ad086a207711",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/50.png",
+            "story_owner": "former"
+        }
+    ],
+    "home_feed": [
+        {
+            "id": "61bb5376-8a0a-4ccb-8193-a0668d75460d",
+            "feed_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png",
+            "like_counts": "board",
+            "about": "sense"
+        },
+        {
+            "id": "590e915b-11bc-4d81-ae8f-d606778e612c",
+            "feed_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png",
+            "like_counts": "form",
+            "about": "west"
+        },
+        {
+            "id": "2c52d49f-70ae-4bde-9da2-77c9b7a4ccf4",
+            "feed_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png",
+            "like_counts": "attention",
+            "about": "might"
+        },
+        {
+            "id": "40541944-adc0-44c2-ba19-ad5899ba79d4",
+            "feed_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png",
+            "like_counts": "the",
+            "about": "expect"
+        },
+        {
+            "id": "67aa3dd7-b14e-412d-9216-35249244c9e8",
+            "feed_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/32.png",
+            "like_counts": "image",
+            "about": "particular"
+        }
+    ]
+}
